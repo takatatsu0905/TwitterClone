cb00282f817a1cf45c146c6dfb8d70afd998630f
takatatsu0905 <takatatsu0905@gmail.com>
2021-07-26 20:19:54 +0900

    ユーザー画面

---
+++ Views/profile.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="..\Views\img\logo-twitterblue.svg">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="..\Views\css\style.css" rel="stylesheet">
+    <!-- js -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous" defer></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" defer></script>
+    <!-- いいね！JS -->
+    <script src="..\Views/js/like.js" defer></script>
+
+    <title>プロフィール画面 / Twitterクローン</title>
+    <meta name="description" content="プロフィール画面です">
+</head>
+
+<body class="home profile text-center">
+    <div class="container">
+        <div class="side">
+            <div class="side-inner">
+                <ul class="nav flex-column">
+                    <li class="nav-item"><a href="home.php" class="nav-link"><img src="..\Views\img\logo-twitterblue.svg" alt="" class="icon"></a></li>
+                    <li class="nav-item"><a href="home.php" class="nav-link"><img src="..\Views\img\icon-home.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="search.php" class="nav-link"><img src="..\Views\img\icon-search.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="notification.php" class="nav-link"><img src="..\Views\img\icon-notification.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="profile.php" class="nav-link"><img src="..\Views\img\icon-profile.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="post.php" class="nav-link"><img src="..\Views\img\icon-post-tweet-twitterblue.svg" alt="" class="post-tweet"></a></li>
+                    <li class="nav-item my-icon"><img src="..\Views\img_uploaded\user\sample-person.jpg" class="js-popover" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="<a href='profile.php'>プロフィール</a> <br> <a href='sign-out.php'>ログアウト</a>" data-bs-html="true"></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="main">
+            <div class="main-header">
+                <h1>太郎</h1>
+            </div>
+
+            <!-- プロフィールエリア -->
+            <div class="profile-area">
+                <div class="top">
+                    <div class="user"><img src="..\Views\img_uploaded\user\sample-person.jpg" alt=""></div>
+
+                    <?php if(isset($_GET['user_id'])) :?>
+                        <!-- 相手のページ -->
+                        <?php if(isset($_GET['case'])) :?>
+                            <button class="btn btn-sm">フォローを外す</button>
+                        <?php else :?>
+                            <button class="btn btn-sm btn-reverse">フォローする</button>
+                        <?php endif;?>
+                    <?php else :?>
+                        <!-- 自分のページ -->
+                        <button class="btn btn-reverse btn-sm" data-bs-toggle="modal" data-bs-target="#js-modal">プロフィール編集</button>
+                    <?php endif ;?>
+                        
+                    <div class="modal fade" id="js-modal" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <form action="profile.php" method="post" enctype="multipart/form-data">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">プロフィールを編集</h5>
+                                        <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="user">
+                                            <img src="..\Views\img_uploaded\user\sample-person.jpg" alt="">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="" class="mb-1">プロフィール写真</label>
+                                            <input type="file" class="form-control form-control-sm" name="image">
+                                        </div>
+                                        <input type="text" class="form-control mb-4" name="nickname" value="太郎" placeholder="ニックネーム" maxlength="50" required>
+                                        <input type="text" class="form-control mb-4" name="name" value="taro" placeholder="ユーザー名" maxlength="50" required>
+                                        <input type="email" class="form-control mb-4" name="email" value="taro@techis.jp" placeholder="メールアドレス" maxlength="254" required>
+                                        <input type="password" class="form-control mb-4" name="password" value="" placeholder="パスワードを変更する場合ご入力ください" minlength="4" maxlength="128">
+                                    </div>
+
+                                    <div class="modal-footer">
+                                        <button class="btn btn-reverse" data-bs-dismiss="modal">キャンセル</button>
+                                        <button calss="btn" type="submit">保存する</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="name">太郎</div>
+                <div class="text-muted">@taro</div>
+
+                <div class="follow-follower">
+                    <div class="follow-count">1</div>
+                    <div class="follow-text">フォロー中</div>
+                    <div class="follow-count">1</div>
+                    <div class="follow-text">フォロワー</div>
+                </div>
+            </div>
+
+            <!-- 仕切りエリア -->
+            <div class="ditch"></div>
+
+            <!-- TODO : つぶやき一覧エリア -->
+        </div>
+    </div>
+</body>
+
+</html>
--- Views/sign-up.php
+++ Views/sign-up.php
@@ -21,7 +21,7 @@
             <input type="text" class="form-control" name="nickname" placeholder="ニックネーム" maxlength="50" required autofocus>
             <input type="text" class="form-control" name="name" placeholder="ユーザー名、例)techis132" maxlength="50" required>
             <input type="email" class="form-control" name="email" placeholder="メールアドレス" maxlength="254" required>
-            <input type="password" class="form-control" name="password" placeholder="パスワード" minlength="4" maxlength="50" required>
+            <input type="password" class="form-control" name="password" placeholder="パスワード" minlength="4" maxlength="128" required>
             <button class="w-100 btn btn-lg" type="submit">登録する</button>
             <p class="mt-3 mb-2"><a href="sign-in.php">ログインする</a></p>
             <p class="mt-2 mb-3 text-muted">&copy; 2021</p>
