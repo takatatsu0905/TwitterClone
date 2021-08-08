34a6fdf7e25152d2f8f22e972932be75e24464e0
takatatsu0905 <takatatsu0905@gmail.com>
2021-08-08 15:09:12 +0900

    ホーム機能

--- Controllers/home.php
+++ Controllers/home.php
@@ -9,6 +9,9 @@ include_once '../confing.php';
 // 便利な関数を読み込む
 include_once '../util.php';
 
+//ツイートデータ操作モデルを読み込む
+include_once '../Models/tweets.php';
+
 //ログインチェック
 $user = getUserSession();
 if(!$user){
@@ -21,31 +24,7 @@ if(!$user){
 $view_user = $user;
 
 //ツイート一覧
-//TODO: モデルから取得する
-$view_tweets = [
-    [
-        'user_id' => 1,
-        'user_name' => 'taro',
-        'user_nickname' => '太郎',
-        'user_image_name' => 'sample-person.jpg',
-        'tweet_body' => '今プログラミングをしています。',
-        'tweet_image_name' => null,
-        'tweet_created_at' => '2021-06-30 15:00:00',
-        'like_id' => null,
-        'like_count' => 0,
-    ],
-    [
-        'user_id' => 2,
-        'user_name' => 'jiro',
-        'user_nickname' => '次郎',
-        'user_image_name' => null,
-        'tweet_body' => 'コワーキングスペースをオープンしました。',
-        'tweet_image_name' => 'sample-post.jpg',
-        'tweet_created_at' => '2021-06-29 15:00:00',
-        'like_id' => 1,
-        'like_count' => 1,
-    ]
-];
+$view_tweets = findTweets($user);
 
 //画面表示
 include_once '../Views/home.php';
